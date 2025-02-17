5b6fe209c2ef3f2dbeff3bba7c1ecc4151b5632f
LatrodectusMactanss <bbellido@insgabrielamistral.cat>
2025-02-17 23:00:54 +0100

    Update Main.java
    tercera

--- src/pt9_git_hub/Main.java
+++ src/pt9_git_hub/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main (String[] args) {
 		System.out.println("1a modificació Git");
 		System.out.println("2a modificació GitHub");
+		System.out.println("3a modificació GitHub");
+
 		
 	}	
 }
