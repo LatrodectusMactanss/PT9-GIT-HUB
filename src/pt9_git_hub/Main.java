6eb301e08f01e3cea454e1d1da647daeebcf5e95
LatrodectusMactanss <bbellido@insgabrielamistral.cat>
2025-02-17 23:14:24 +0100

    Update Main.java
    quinta

--- src/pt9_git_hub/Main.java
+++ src/pt9_git_hub/Main.java
@@ -6,5 +6,6 @@ public class Main {
 		System.out.println("2a modificació GitHub");
 		System.out.println("3a modificació GitHub");
 		System.out.println("4a modificació GitHub");
+		System.out.println("5a modificació GitHub");
 	}	
 }
